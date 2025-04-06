f1d706014e63db6f455d70e34954b5e4a03cc009
ignaciopiccolo <igpiccolovazquez@mail.austral.edu.ar>
2025-04-06 14:05:08 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,10 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto = money - expense
+    pesos = int(vuelto)
+    centavos = int(round((vuelto - pesos) * 100))
+    print(expense)
+    print(int(money))
+    print(pesos)
+    print(centavos)
