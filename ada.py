437473def0044fe4ee63f1dc1d49d83e0f1d0b2e
ignaciopiccolo <igpiccolovazquez@mail.austral.edu.ar>
2025-04-06 14:02:12 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,11 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    lower_case = f"{first_name.lower()} {last_name.lower()}"
+    title_case = f"{first_name.title()} {last_name.title()}"
+    upper_case = f"{first_name.upper()} {last_name.upper()}"
+    tabbed_lower_case = f"\t{first_name.lower()} {last_name.lower()}"
+    print(lower_case)
+    print(title_case)
+    print(upper_case)
+    print(tabbed_lower_case)
