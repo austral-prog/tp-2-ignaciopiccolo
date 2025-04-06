a560555fdd53096a3a9efc6994f4f0f34d4cd0a1
ignaciopiccolo <igpiccolovazquez@mail.austral.edu.ar>
2025-04-06 14:55:07 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,8 +1,6 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
-    x_primero = x < y
-    y_primero = y < x
-    print(f"¿{x} viene antes que {y} en el diccionario? {x_primero}.")
-    print(f"¿{y} viene antes que {x} en el diccionario? {y_primero}.")
-earth()     
+    print(f"The result of {x} comes first in the dictionary than {y} is {x < y}.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {y < x}.")
+earth()
