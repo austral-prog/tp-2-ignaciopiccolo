b56c1c7f2cdb69e86c3921f68d897b770d74e330
ignaciopiccolo <igpiccolovazquez@mail.austral.edu.ar>
2025-04-06 14:29:53 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -5,3 +5,4 @@ def earth():
     y_primero = y < x
     print(f"¿{x} viene antes que {y} en el diccionario? {x_primero}.")
     print(f"¿{y} viene antes que {x} en el diccionario? {y_primero}.")
+earth()     
